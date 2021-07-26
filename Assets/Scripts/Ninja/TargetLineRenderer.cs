ee53d41ca14e1679bb5d7090a6c70376732f2916
nNormanLee <61098993+nNormanLee@users.noreply.github.com>
2021-07-26 03:21:13 -0500

    Scaling and Line Render
    sizes and physics scaled to normal play. player movement script and TargetLineRenderer scripts adjusted/written for Ninja navigation.

---
+++ Assets/Scripts/Ninja/TargetLineRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLineRenderer : MonoBehaviour
+{
+    
+    public PlayerMovement playerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, playerMovement.attractor.transform.position);
+        lineRenderer.SetPosition(1, playerMovement.FindJumpPoint());
+        lineRenderer.SetPosition(2, playerMovement.target.transform.position);
+
+    }
+}
